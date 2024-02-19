6cf4efcefc643aec1c07cf87f138f3d339f48563
Lucas Dantas Nogueira <lucas.dantas.nogueira@gmail.com>
2024-02-19 02:07:02 -0300

    feat: created AuthController tests and logic

--- backend/app/Http/Controllers/Api/V1/AuthController.php
+++ backend/app/Http/Controllers/Api/V1/AuthController.php
@@ -3,34 +3,40 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+
+    use HttpResponses;
+
     public function login(Request $request)
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required|max:255',
-            'email' => 'required|unique|max:255',
-            'password' => 'required',
+            'email' => 'required|max:255',
+            'password' => 'required|min:6',
         ]);
 
         if ($validator->fails()) {
-            // mensagem de erro   
-            return response()->json(['message' => 'erro']);
+            return $this->error('Erro nos valores adicionados aos campos!', 400);
         }
 
-        if (!Auth::attempt($validator->validated())) {
-            // mensagem de erro
-            return response()->json(['message' => 'erro']);
+        $credentials = [
+            'email' => $validator->validated()['email'],
+            'password' => $validator->validated()['password'],
+        ];
+
+        if (!Auth::attempt($credentials)) {
+            return $this->response('Email ou Senha inválidos!', 422);
         }
 
-        // mensagem de sucesso
-        return response()->json(['message' => 'sucesso']);
-
-        
+        return $this->response('Authorized', 200, [
+            'token' => $request->user()->createToken('app')->plainTextToken,
+        ]);
     }
 
     public function logout()
--- backend/tests/Feature/Api/AuthController/AuthControllerTest.php
+++ backend/tests/Feature/Api/AuthController/AuthControllerTest.php
@@ -1,20 +1,42 @@
 <?php
 
+use App\Models\User;
+
 uses()->group('auth');
 
 it('user login', function () {
+    $user = User::factory()->create()->first();
+
     $payload = [
-        'name' => 'teste',
-        'email' => 'teste@gmail.com',
-        'password' => 'teste123',
+        'name' => $user->name,
+        'email' => $user->email,
+        'password' => 'password',
     ];
 
     $response = $this->post('/api/v1/login', $payload);
-
     $response->assertStatus(200);
-    // $response = $this->get('/api/usercontroller/usercontroller');
+});
 
-    // $response->assertStatus(200);
+it('invalid user credentials login', function () {
+    $payload = [
+        'name' => 'teste erro',
+        'email' => 'teste.erro@example.com',
+        'password' => 'testeerro',
+    ];
+
+    $response = $this->post('/api/v1/login', $payload);
+    $response->assertStatus(422);
+});
+
+it('invalid input for login', function () {
+    $payload = [
+        'name' => 'teste erro',
+        'email' => 'teste.erro@example.com',
+        'password' => 'teste',
+    ];
+
+    $response = $this->post('/api/v1/login', $payload);
+    $response->assertStatus(400);
 });
 
 it('user logout', function () {
